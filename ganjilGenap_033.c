66244a81bce3fb332adf7a5eec0b8448efebd60d
Rivald0 <rivaldo2butar@gmail.com>
2022-09-21 16:41:44 +0700

    Projek Ku

---
+++ ganjilGenap_033.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char * argv[])
+{
+    int x = atoi(argv[1]);
+    if (x%2==0)
+    {
+        printf("%d bilangan genap\n", x);
+    }
+    else
+    {
+        printf("%d bilangan ganjil\n", x);
+    }
+    
+    getchar();
+    return 0;
+}
