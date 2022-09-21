ac2a2fe8ab4905ec4620f0d9ae6b9b132cb7ef02
Rivald0 <rivaldo2butar@gmail.com>
2022-09-21 17:31:56 +0700

    masuk

--- ganjilGenap_033.c
+++ ganjilGenap_033.c
@@ -8,14 +8,9 @@ int main (int argc, char * argv[])
     printf("Masukkan Sebuah Bilangan Genap: ");
     scanf ("%d", &x);
     if (x%2==0)
-    {
         printf("%d bilangan genap\n", x);
-    }
     else
-    {
         printf("%d bilangan ganjil\n", x);
-    }
-    
     getch();
     return 0;
 }
---
+++ grade_033.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    //kamus
+    float x;
+    char ch;
+
+
+    //algoritma
+    printf("Masukkan nilai Kuliah Anda: ");
+    scanf("%f", %x);
+    if (x>80) ch = A;
+    else if (x>=65) ch = B;
+    else if (x>=50) ch = C;
+    else if (x>=35) ch = D;
+    else ch = 'E';
+    printf("Anda mendapatkan grade %c\n", ch);
+    getch();
+
+    return 0;
+}
